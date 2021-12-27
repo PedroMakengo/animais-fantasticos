6071ad8a6d1c98f84710c365afeeb30e34ee181f
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2021-12-27 12:52:22 -0300

    Trabalhando na refatoração das minhas funções

--- js/modules/scroll-suave.js
+++ js/modules/scroll-suave.js
@@ -1,31 +1,32 @@
-export default function initScrollSuave() {
-  const linksInternos = document.querySelectorAll(
-    '[data-menu="suave"] a[href^="#"]',
-  );
+export default class ScrollSuave {
+  constructor(links, options) {
+    this.linksInternos = document.querySelectorAll(links);
 
-  function scrollToSection(event) {
-    event.preventDefault();
+    if (options === undefined) {
+      this.options = { behavior: 'smooth', block: 'start' };
+    } else {
+      this.options = options;
+    }
 
-    const href = event.currentTarget.getAttribute('href');
-
-    const section = document.querySelector(href);
-
-    section.scrollIntoView({
-      behavior: 'smooth',
-      block: 'start',
-    });
-
-    // forma alternativa
-    // const topo = section.offsetTop;
-    // window.scrollTo({
-    //   top: topo,
-    //   behavior: "smooth"
-    // })
+    this.scrollToSection = this.scrollToSection.bind(this);
   }
 
-  if (linksInternos.length) {
-    linksInternos.forEach((link) => {
-      link.addEventListener('click', scrollToSection);
+  scrollToSection(event) {
+    console.log(this);
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute('href');
+    const section = document.querySelector(href);
+
+    section.scrollIntoView(this.options);
+  }
+
+  addLinkEvent() {
+    this.linksInternos.forEach((link) => {
+      link.addEventListener('click', this.scrollToSection);
     });
+  }
+
+  init() {
+    this.addLinkEvent();
   }
 }

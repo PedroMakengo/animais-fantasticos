9eef9b2868d57f0ce0d76ae4ac4af3401123b609
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2022-01-12 17:04:33 -0300

    Trabalhado final

--- js/script.js
+++ js/script.js
@@ -9,6 +9,7 @@ import Funcionamento from './modules/funcionamento.js';
 import fetchAnimais from './modules/fetch-animais.js';
 import fetchBitcoin from './modules/fetch-bitcoin.js';
 import ScrollAnima from './modules/scroll-anima.js';
+import SlideNav from './modules/slider.js';
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
@@ -31,15 +32,16 @@ scrollAnima.init();
 const dropdown = new DropdownMenu('[data-dropdown]');
 dropdown.init();
 
-
 const menuMobile = new MenuMobile('[data-menu="button"]', '[data-menu="list"]');
 menuMobile.init();
 
 const funcionamento = new Funcionamento('[data-semana]', 'aberto');
 funcionamento.init();
 
-
 fetchAnimais('../../animaisapi.json', '.numeros-grid');
 
-
 fetchBitcoin('https://blockchain.info/ticker', '.btc-preco');
+
+const slide = new SlideNav('.slide', '.slide-wrapper');
+slide.init();
+slide.addControl('.custom-controls');
